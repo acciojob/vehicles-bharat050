dba066029df7b33fc1ebe49d11c78e8835c8264e
Bharat <aimhigh050@gmail.com>
2024-01-09 21:47:14 +0530

    changed steer method2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,33 +2,33 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-//        F1 mercedes = new F1("mercedes", false);
-//        mercedes.move(40, 0);
-//        mercedes.accelerate(20);
-//        mercedes.accelerate(-60);
+        F1 mercedes = new F1("mercedes", false);
+        mercedes.move(40, 0);
+        mercedes.accelerate(20);
+        mercedes.accelerate(-60);
 
-        Vehicle vehicle = new Vehicle("Truck");
-
-//        assert vehicle.getName() == "Truck";
-
-//        assert vehicle.getCurrentDirection() == 0;
-
-//        assert vehicle.getCurrentSpeed() == 0;
-
-        vehicle.move(10, 45);
-
-//        assert vehicle.getCurrentSpeed() == 10;
+//        Vehicle vehicle = new Vehicle("Truck");
 //
-//        assert vehicle.getCurrentDirection() == 45;
-
-        vehicle.steer(45);
-
-//        assert vehicle.getCurrentDirection() == 90;
+////        assert vehicle.getName() == "Truck";
 //
-//        assert vehicle.getCurrentSpeed() == 10;
-
-        vehicle.stop();
-
-//        assert vehicle.getCurrentSpeed() == 0;
+////        assert vehicle.getCurrentDirection() == 0;
+//
+////        assert vehicle.getCurrentSpeed() == 0;
+//
+//        vehicle.move(10, 45);
+//
+////        assert vehicle.getCurrentSpeed() == 10;
+////
+////        assert vehicle.getCurrentDirection() == 45;
+//
+//        vehicle.steer(45);
+//
+////        assert vehicle.getCurrentDirection() == 90;
+////
+////        assert vehicle.getCurrentSpeed() == 10;
+//
+//        vehicle.stop();
+//
+////        assert vehicle.getCurrentSpeed() == 0;
     }
 }
